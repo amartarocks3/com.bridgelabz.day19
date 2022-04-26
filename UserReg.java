dbb03bd2370853b9cd9e56ec174d29afffc86cc1
amartarocks3 <amartarocks3@gmail.com>
2022-04-26 18:23:38 +0530

    added: Validation for user registration for firstName

--- UserReg.java
+++ UserReg.java
@@ -7,7 +7,7 @@ public class UserReg {
 	public static void main(String[] args) {
 		System.out.println("Enter a user name");
 		Scanner sc = new Scanner(System.in);
-		boolean result = Email(sc.nextLine());
+		boolean result = FirstName(sc.nextLine());
 		if(result) {
 			System.out.println("User registration is valid");
 		}
@@ -19,8 +19,5 @@ public class UserReg {
 		
 		return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
 	}
-	public static boolean Email(String eMail) {
-		return eMail.matches("^[a-z]{3}[A-Za-z0-9+_.-]*@[a-z]{2}[.]{1}[a-z]{2}[.]*[a-z]*$");
-	}
-
+	
 }
