0aec736954b1cf8f50a4538f5ea10c1388262871
amartarocks3 <amartarocks3@gmail.com>
2022-04-27 22:53:16 +0530

    Added: Validation for phone number

--- UserReg.java
+++ UserReg.java
@@ -1,46 +1,73 @@
 package com.bl.userRegistration;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class UserReg {
+    //    First name pattern
+    public static void checkFirstName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first name: ");
+        String firstName = sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName)) {
+            System.out.println( "Valid entry.\n");
+        } else {
+            System.err.println("Invalid entry!\n-First letter should be capital,other letters should be small" +
+                    "\n-First name should contain at least 3 letters\n");
+            checkFirstName();
+        }
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Enter first name");
-		Scanner sc = new Scanner(System.in);
-		boolean resultFirst = FirstName(sc.nextLine());
-		System.out.println("Enter last name");
-		boolean resultLast = LastName(sc.nextLine());
-		System.out.println("Enter Email id");
-		boolean resultemail = Email(sc.nextLine());
-		if(resultFirst) {
-			System.out.println("User registration is valid");
-		}
-		else {
-			System.out.println("User registration is invalid");
-		}
-		if(resultLast) {
-			System.out.println("User registration is valid");
-		}
-		else {
-			System.out.println("User registration is invalid");
-		}
-		if(resultemail) {
-			System.out.println("User registration is valid");
-		}
-		else {
-			System.out.println("User registration is invalid");
-		}
-		}
-	public static boolean FirstName(String firstName) {
-		
-		return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
-	}
-	public static boolean LastName(String lastName) {
-		
-		return lastName.matches("^[A-Z]{1}[a-z]{2,}$");
-	}
-	public static boolean Email(String eMail) {
-		return eMail.matches("^[a-z]{3}[A-Za-z0-9+_.-]*@[a-z]{2}[.]{1}[a-z]{2}[.]*[a-z]*$");
-	}
+    //    Last name pattern
+    public static void checkLastName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Last name: ");
+        String lastName = sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName)) {
+            System.out.println("valid entry.\n");
+        } else {
+            System.err.println("Invalid entry!\n-First letter should be capital" +
+                    "\n-Last name should contain at least 3 letters\n");
+            checkLastName();
+        }
+    }
 
+    //    EmailID pattern
+    public static void checkEmail() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Email : ");
+        String email = sc.nextLine();
+        if(Pattern.matches("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*[@][a-z]+[.][a-z]{2,3}([.][a-z]{2})?$",email)){
+            System.out.println("Valid entry.\n");
+        }
+        else {
+            System.err.println("Invalid entry!\n-user name should have at least 3 letters" +
+                    "\n-It should contain . after company name, company domain" +
+                    "\n-Company name should be 2 letters" +
+                    "\n-Company domain should be 2 letters" );
+            checkEmail();
+        }
+    }
+
+    public static void checkMobileNo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Phone No : ");
+        String mobileNo = sc.nextLine();
+        if( Pattern.matches("[0-9]{2}+[ ][0-9]{10}$",mobileNo)){
+            System.out.println("Valid entry.");
+        }
+        else {
+            System.err.println("Invalid entry!\n-Mobile phone no. should contain 12 digits including country code" +
+                    "\n-It should contain only digits");
+            checkMobileNo();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        checkFirstName();
+        checkLastName();
+        checkEmail();
+        checkMobileNo();
+    }
 }
